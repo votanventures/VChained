bc100d5d1fc3e7d6fb2b2654570fb9bff2978d2d
rishav-votanventures <88194946+rishav-votanventures@users.noreply.github.com>
2022-02-17 19:01:56 +0530

    fixing wallet connection

--- module/src/services/BlockchainServices.ts
+++ module/src/services/BlockchainServices.ts
@@ -1,37 +1,106 @@
-import { TempleWallet } from "@temple-wallet/dapp";
+import {
+  WalletClient,
+  BeaconMessageType,
+  PermissionScope,
+  PermissionResponseInput,
+  BeaconErrorType,
+  BeaconResponseInputMessage
+} from "@airgap/beacon-sdk";
 
-(async () => {
-  try {
-    const available = await TempleWallet.isAvailable();
-    if (!available) {
-      throw new Error("Temple Wallet not installed");
-    }
+const connectApp = async (): Promise<void> => {
+  // Only ONE WalletClient should be created and reused everywhere.
+  const client = new WalletClient({ name: "My Wallet" });
+  await client.init(); // Establish P2P connection
 
-    // Note:
+  client
+    .connect(async (message) => {
+      console.log("beacon message", message);
 
-    // use `TempleWallet.isAvailable` method only after web application fully loaded.
+      let response: BeaconResponseInputMessage;
+      switch (message.type) {
+        case BeaconMessageType.PermissionRequest:
+          // Prompt user for permissions. If multiple accounts are available, let user choose one of them
 
-    // Alternatively, you can use the method `TempleWallet.onAvailabilityChange`
-    // that tracks availability in real-time .
+          response = {
+            type: BeaconMessageType.PermissionResponse,
+            network: message.network, // Use the same network that the user requested
+            scopes: message.scopes,
+            id: message.id,
+            publicKey:
+              "3b92229274683b311cf8b040cf91ac0f8e19e410f06eda5537ef077e718e0024",
+          };
 
-    const wallet = new TempleWallet("My Super DApp");
-    await wallet.connect("carthagenet");
-    const tezos = wallet.toTezos();
+          // Optional
+          // If the user aborts an action at any time, you can send back an error:
+          response = {
+            type: BeaconMessageType.Error,
+            id: message.id,
+            errorType: BeaconErrorType.ABORTED_ERROR,
+          };
 
-    const accountPkh = await tezos.wallet.pkh();
-    const accountBalance = await tezos.tz.getBalance(accountPkh);
-    console.info(`address: ${accountPkh}, balance: ${accountBalance}`);
+          break;
+        case BeaconMessageType.SignPayloadRequest:
+          // Sign message.payload and return signature
 
-    const counter = await tezos.wallet.at(
-      "KT1DjYkruvfujfKw6nLYafArqKufcwHuKXvT"
-    );
+          response = {
+            type: BeaconMessageType.SignPayloadResponse,
+            id: message.id,
+            signingType: message.signingType,
+            signature: "edsig...",
+          };
 
-    const operation = await counter.methods.increment(1).send();
-    await operation.confirmation();
+          break;
+        case BeaconMessageType.OperationRequest:
+          // Prepare transaction based on the details give in message.operationDetails
+          // message.operationDetails only contains a partial tezos transaction. Not all fields are mandatory
+          // The fields that are not present (eg. counter, fee, gas_limit) have to be set by the wallet
+          // If one of the optional fields is set, then that one should be used and not replaced by the wallet
 
-    const counterValue = await counter.storage();
-    console.info(`count: ${counterValue}`);
-  } catch (err) {
-    console.error(err);
-  }
-})();
+          response = {
+            type: BeaconMessageType.OperationResponse,
+            id: message.id,
+            transactionHash: "op...",
+          };
+
+          // Optional
+          // If the transaction cannot be prepared, (eg. run_operations fails), an error containing the rpc error can be returned
+          let error = [
+            {
+              kind: "temporary",
+              id: "proto.008-PtEdo2Zk.contract.balance_too_low",
+              contract: "tz1...",
+              balance: "100",
+              amount: "200",
+            },
+          ]; // RPC error
+          response = {
+            type: BeaconMessageType.Error,
+            id: message.id,
+            errorType: BeaconErrorType.TRANSACTION_INVALID_ERROR,
+            errorData: error,
+          };
+
+          break;
+        case BeaconMessageType.BroadcastRequest:
+          response = {
+            type: BeaconMessageType.OperationResponse,
+            id: message.id,
+            transactionHash: "op...",
+          };
+
+          break;
+        default:
+          response = {
+            type: BeaconMessageType.Error,
+            id: (message as any).id, // Typescript assumes message is of type "never", but we should still add this to handle any potential future messages we don't support yet.
+            errorType: BeaconErrorType.ABORTED_ERROR,
+          };
+      }
+
+      client.respond(response);
+    })
+    .catch((error) => console.error("connect error", error));
+};
+
+connectApp().catch((error) => console.error("connect error", error));
+
